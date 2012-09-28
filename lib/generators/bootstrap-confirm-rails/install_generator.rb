848be0a5ebaf9af2a8fffc7a2a1b5d175a3425ef
Spring MC <Heresy.Mc@gmail.com>
2012-09-28 17:49:54 +0800

    fixed typo error

--- lib/generators/bootstrap-confirm-rails/install_generator.rb
+++ lib/generators/bootstrap-confirm-rails/install_generator.rb
@@ -3,7 +3,7 @@ module BootstrapConfirmRails
     class InstallGenerator < Rails::Generators::Base
       desc 'Setup BootstrapConfirmRails default files.'
 
-      source_root File.expend_path('../templates', __FILE__)
+      source_root File.expand_path('../templates', __FILE__)
 
       def copy_config
         template File.join('config', 'initializers', 'bootstrap-confirm-rails.rb')
