9a0c5ee0ad68afbdaf0e0174b9dff185e4517a5e
Spring MC <Heresy.Mc@gmail.com>
2012-09-28 16:55:28 +0800

    remove unused dummy folder

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '298f19cf8e34b7cce9a6052e1e5d979c684adc4bdb9ff79cbaab8a4699d7917e2f36ed3abf1afbeaacd67465cbe627eab612c7019901a7daa412472849a4f4ba'
